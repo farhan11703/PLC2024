07147413d4ce995fa26304e6b03bea511d783807
Farhan Rahman <farhan.r@outlook.ie>
2024-04-28 14:35:30 +0000

    lab2 done

--- lab2-functionalprimer/haskell/task2.hs
+++ lab2-functionalprimer/haskell/task2.hs
@@ -1,4 +1,9 @@
+onePlusone = do
+    print ("1 + 1 = 2")
+
+
 main = do
     putStrLn "Welcome to the programme. Please enter your name"
     name <- getLine
     putStrLn("Hello " ++ name ++ ", hope you like Haskell.")
+    onePlusone
--- lab2-functionalprimer/haskell/task4.hs
+++ lab2-functionalprimer/haskell/task4.hs
@@ -4,9 +4,11 @@ ask prompt =
   putStrLn prompt
   line <- getLine
   if line == ""
-    then ask prompt
+    then ask (prompt ++ "!")
+    else if line == "quit"
+    then putStrLn ("quitting...")
     else putStrLn ("you said: " ++ reverse line)
-
+  
 main :: IO ()
 main =
   do
